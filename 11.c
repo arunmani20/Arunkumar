a3b7c9b5db7586e18505aa43faa27848058e959e
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-03-15 08:38:03 +0000

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+char s[20];
+ printf("\nEnter a day in small letters : ");
+ scanf("%s",s);
+if(s[0]=='s' || s[0]=='s')
+{
+    printf("holiday");
+}
+else
+{
+    printf("working day");
+}
+}
