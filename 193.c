ac57f220ddc5fd53c17e2d480a40852c21a835bc
arunmani20 <30829932+arunmani20@users.noreply.github.com>
2018-03-20 16:03:11 +0530

    Create 193.c

---
+++ 193.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,x,y;
+    scanf("%d %d %d",&a,&b,&x);
+    printf("\n The values of A,band X are %d %d %d",a,b,x);
+    y=(a*x)+b;
+    printf("\n The value of Y is %d",y);
+}
